02898f30d67a78e7e558d18f0547f377b0f8cbe4
Alex Silva <alexcssilva@gmail.com>
2022-01-03 23:17:28 -0300

    Desafio 03 concluido

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React from 'react';
+import Title from './Title';
 
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div data-testid="solar-system"></div>
+      <div data-testid='solar-system'>
+        <Title />
+      </div>
     );
   }
 }
+
+export default SolarSystem;
